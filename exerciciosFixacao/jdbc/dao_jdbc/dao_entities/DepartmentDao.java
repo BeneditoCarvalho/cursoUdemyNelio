1538bcbae595c3de487c4d16bbaee87033625771
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-08-02 09:25:52 -0300

    Entities DAO

---
+++ exerciciosFixacao/jdbc/dao_jdbc/dao_entities/DepartmentDao.java
@@ -0,0 +1,18 @@
+package cursoUdemyNelio.exerciciosFixacao.jdbc.dao_jdbc.dao_entities;
+
+import cursoUdemyNelio.exerciciosFixacao.jdbc.dao_jdbc.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    void insert(Department obj);
+
+    void update(Department obj);
+
+    void deleteById(Integer id);
+
+    Department findById(Integer id);
+
+    List<Department> findAll();
+}
