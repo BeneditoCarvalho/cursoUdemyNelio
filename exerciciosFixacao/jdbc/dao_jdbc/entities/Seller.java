617b6bce7740c1e815fbf0503b44a5e28de4c9f5
Benedito Carvalho <benedito.carvalho@gmail.com>
2021-08-02 08:56:40 -0300

    AulaDB DAO-JDBC 01 (correção)

--- exerciciosFixacao/jdbc/dao_jdbc/entities/Seller.java
+++ exerciciosFixacao/jdbc/dao_jdbc/entities/Seller.java
@@ -1,16 +1,21 @@
 package cursoUdemyNelio.exerciciosFixacao.jdbc.dao_jdbc.entities;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
-public class Seller {
+public class Seller implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     private Integer id;
     private String name;
     private String email;
     private Date birthDate;
     private Double baseSalary;
-    Department department;
+    private Department department;
 
     public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
         this.id = id;
@@ -61,16 +66,36 @@ public class Seller {
         this.baseSalary = baseSalary;
     }
 
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Seller seller = (Seller) o;
-        return id.equals(seller.id) && name.equals(seller.name);
+        return id.equals(seller.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", baseSalary=" + baseSalary +
+                ", department=" + department +
+                '}';
     }
 }
